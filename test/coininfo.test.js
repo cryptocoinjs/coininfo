cb59823a155222b0b9981ffe9c4712a503d612bb
JP Richardson <jprichardson@gmail.com>
2015-01-02 21:41:05 -0600

    coininfo: support reading from coin files

--- test/coininfo.test.js
+++ test/coininfo.test.js
@@ -3,22 +3,33 @@ var ci = require('../')
 
 
 describe('+ coininfo()', function() {
-  it('should return the version', function() {
-    var v = ci("LTC").versions
-    assert.equal(v.public, 0x30)
-    assert.equal(v.private, 0xB0)
+
+
+  describe('versions', function() {
+    it('should return the version', function() {
+      var v = ci("LTC").versions
+      assert.equal(v.public, 0x30)
+      assert.equal(v.private, 0xB0)
+    })
+
+    it('should return bip32', function() {
+      var v = ci('BTC-TEST').versions.bip32
+      assert.equal(v.public, 0x043587cf)
+      assert.equal(v.private, 0x04358394)
+    })
+
+    describe('> when does not have bip32', function() {
+      it('should return null', function() {
+        var v = ci('PPC').versions.bip32
+        assert(!v)
+      })
+    })
   })
 
-  it('should return bip32', function() {
-    var v = ci('BTC-TEST').versions.bip32
-    assert.equal(v.public, 0x043587cf)
-    assert.equal(v.private, 0x04358394)
-  })
-
-  describe('> when does not have bip32', function() {
+  describe('> when coin not found', function() {
     it('should return null', function() {
-      var v = ci('PPC').versions.bip32
-      assert(!v)
+      var info = ci('XXX')
+      assert.equal(info, null)
     })
   })
 })

f7feb27917ccea820d3e61f7f23dd20a564f0bb7
JP Richardson <jprichardson@gmail.com>
2015-04-20 17:22:27 -0500

    JavaScript Standard Style

--- test/coininfo.test.js
+++ test/coininfo.test.js
@@ -1,9 +1,10 @@
 var assert = require('assert')
 var ci = require('../')
 
+/* global describe, it */
 
-describe('+ coininfo()', function() {
-  describe('iterate all coins', function() {
+describe('+ coininfo()', function () {
+  describe('iterate all coins', function () {
     var coins = [
       'blk',
       'btc', 'btc-test',
@@ -17,36 +18,36 @@ describe('+ coininfo()', function() {
       'uro', 'uro-test'
     ]
 
-    coins.forEach(function(c) {
-      it('should return valid data for ' + c, function() {
+    coins.forEach(function (c) {
+      it('should return valid data for ' + c, function () {
         assert(ci(c).versions.scripthash)
       })
     })
   })
 
-  describe('versions', function() {
-    it('should return the version', function() {
-      var v = ci("LTC").versions
+  describe('versions', function () {
+    it('should return the version', function () {
+      var v = ci('LTC').versions
       assert.equal(v.public, 0x30)
       assert.equal(v.private, 0xB0)
     })
 
-    it('should return bip32', function() {
+    it('should return bip32', function () {
       var v = ci('BTC-TEST').versions.bip32
       assert.equal(v.public, 0x043587cf)
       assert.equal(v.private, 0x04358394)
     })
 
-    describe('> when does not have bip32', function() {
-      it('should return null', function() {
+    describe('> when does not have bip32', function () {
+      it('should return null', function () {
         var v = ci('PPC').versions.bip32
         assert(!v)
       })
     })
   })
 
-  describe('> when coin not found', function() {
-    it('should return null', function() {
+  describe('> when coin not found', function () {
+    it('should return null', function () {
       var info = ci('XXX')
       assert.equal(info, null)
     })
